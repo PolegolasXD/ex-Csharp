1314ccdd7813287a38d1ba86d6b5208e7d924d3b
4lysson-a <4ly.alcantara@gmail.com>
2022-03-09 20:28:22 -0300

    feat: adicionando os primeiros ex em CS.

---
+++ ex_1.cs
@@ -0,0 +1,37 @@
+using System;
+
+class bascara
+{
+  static void Main()
+  {
+    double a = 0, b, c, delta, x_1, x_2;
+
+    Console.WriteLine("Digite o primeiro valor: ");
+    try
+    {
+      a = Single.Parse(Console.ReadLine());
+      Console.WriteLine("Digite o segundo valor: ");
+      b = Single.Parse(Console.ReadLine());
+      Console.WriteLine("Digite o terceiro valor: ");
+      c = Single.Parse(Console.ReadLine());
+
+      //Processo
+      delta = Math.Pow(b, 2) - (4 * a * c);
+      Console.WriteLine("O valor de delta é: " + delta);
+
+      //Processo 2 
+      x_1 = ((-b + Math.Sqrt(delta)) / (2 * a));
+      x_2 = ((-b - Math.Sqrt(delta)) / (2 * a));
+
+      Console.WriteLine("Valor x' " + x_1);
+      Console.WriteLine("x'' " + x_2);
+    }
+    catch
+    {
+      Console.WriteLine("OPS :( Algo deu errado! ");
+      System.Environment.Exit(1);
+    }
+
+
+  }
+}
