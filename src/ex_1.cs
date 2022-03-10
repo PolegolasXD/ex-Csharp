734a6cd5094843e33b0cc9b5e1ec69f3199d3cb2
4lysson-a <4ly.alcantara@gmail.com>
2022-03-09 22:14:48 -0300

    feat: create a ex_1 table and change path structure

---
+++ src/ex_1.cs
@@ -0,0 +1,57 @@
+using System;
+
+class chapim
+{
+  static void Main()
+  {
+    try
+    {
+
+      float soma_idade = 0;
+      float media_idade = 0;
+
+      int[] idade_aluno = new int[4];
+      string[] nome_aluno = new string[4];
+
+      for (int x = 0; x < 4; x++)
+      {
+        Console.Clear();
+        Console.WriteLine("\nNome do " + (x + 1) + " aluno: \n");
+        nome_aluno[x] = Console.ReadLine();
+
+        Console.WriteLine("\nIdade do " + (x + 1) + " aluno: \n");
+        idade_aluno[x] = int.Parse(Console.ReadLine());
+      }
+
+
+      for (int x = 0; x < 4; x++)
+      {
+        soma_idade += idade_aluno[x];
+      }
+
+      media_idade = soma_idade / 5;
+
+
+
+      Console.Clear();
+      Console.WriteLine(" ----- Cálculo da média dos alunos  ----- ")
+      Console.WriteLine(" id" + " --- " + "Nome do aluno" + "  --- " + "    idade do aluno\n");
+
+      for (int x = 0; x < 4; x++)
+      {
+        int size_14 = (nome_aluno[x].Length - 14) * -1;
+        string space_string = new string(' ', size_14);
+
+        Console.WriteLine(" " + x + 1 + "  --- " + nome_aluno[x] + space_string + " ---     " + idade_aluno[x]);
+      }
+
+      Console.WriteLine("\n A média das idades é: " + media_idade);
+
+    }
+    catch
+    {
+      Console.WriteLine(" OPS :(  Algo deu errado!!");
+    }
+  }
+}
+
